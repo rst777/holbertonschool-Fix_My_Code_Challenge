4e35bfb9e90a4eb6acd1cf6b28f527e6ec026abb
Your Name <you@example.com>
2025-01-13 13:32:39 +0100

    0-fizzbuzz.py

--- challenge/0-fizzbuzz.py
+++ challenge/0-fizzbuzz.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 """ FizzBuzz
 """
-
 import sys
+
 
 def fizzbuzz(n):
     """
@@ -12,17 +12,21 @@ def fizzbuzz(n):
       multiples of five print "Buzz".
     - For numbers which are multiples of both three and five print "FizzBuzz".
     """
+    if n < 1:
+        return
 
+    tmp_result = []
     for i in range(1, n + 1):
-        if i % 3 == 0 and i % 5 == 0:
-            print("FizzBuzz", end=" ")
-        elif i % 3 == 0:
-            print("Fizz", end=" ")
-        elif i % 5 == 0:
-            print("Buzz", end=" ")
+        if (i % 3) == 0 and (i % 5) == 0:
+            tmp_result.append("FizzBuzz")
+        elif (i % 3) == 0:
+            tmp_result.append("Fizz")
+        elif (i % 5) == 0:
+            tmp_result.append("Buzz")
         else:
-            print(i, end=" ")
-    print()  # Pour ajouter une nouvelle ligne a la fin
+            tmp_result.append(str(i))
+    print(" ".join(tmp_result))
+
 
 if __name__ == '__main__':
     if len(sys.argv) <= 1:
